05446b829a5f09e975da1784a2a39102ab2ef54f
Ashish Lalwaney <junk.ashish.lalwaney@gmail.com>
2023-03-20 15:01:26 +0530

    LikelihoodFUnctionForMM

---
+++ LikelihoodFunctionForMixtureModels.R
@@ -0,0 +1,8 @@
+n     = 100           # Size of the sample to be generated
+w     = c(0.3, 0.25, 0.25, 0.2)  # Weights
+mu    = c(1, 4, 7, 10)      # Means
+cc    = sample(1:4, n, replace=T, prob=w)
+x     = rexp(n, 1/ mu[cc])
+
+mean =  mean(x)    #5.05
+var = var(x)
