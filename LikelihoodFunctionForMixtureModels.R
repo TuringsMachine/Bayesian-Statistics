67b8fc9565b0616a8c9a1a277e6d8c2f90b38c10
Ashish Lalwaney <junk.ashish.lalwaney@gmail.com>
2023-03-20 15:02:46 +0530

    Likelihood Function For MM

--- LikelihoodFunctionForMixtureModels.R
+++ LikelihoodFunctionForMixtureModels.R
@@ -3,6 +3,6 @@ w     = c(0.3, 0.25, 0.25, 0.2)  # Weights
 mu    = c(1, 4, 7, 10)      # Means
 cc    = sample(1:4, n, replace=T, prob=w)
 x     = rexp(n, 1/ mu[cc])
-
+#calculating overall mean and variance
 mean =  mean(x)    #5.05
 var = var(x)
